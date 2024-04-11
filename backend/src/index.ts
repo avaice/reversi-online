6603b5b9f56e2e5e69fa4770e799621804b373dc
avaice <dev@cho-ice.xyz>
2024-04-11 21:30:42 +0900

    CORS設定の修正

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,7 @@ const app = express()
 const server = createServer(app)
 const io = new Server(server, {
   cors: {
-    origin: process.env.CORS_ORIGIN || "*.online-reversi.xyz",
+    origin: process.env.CORS_ORIGIN || "online-reversi.xyz",
   },
 })
 
