e4aba890c61ab0f6f8d795d518320b69f6271995
avaice <dev@cho-ice.xyz>
2024-04-12 13:55:45 +0900

    フロントエンドにSentryを追加

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,20 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import * as Sentry from '@sentry/react';
 import 'the-new-css-reset/css/reset.css';
 import './index.css';
+import { ENV } from './modules/env.ts';
+
+Sentry.init({
+  dsn: ENV.CENTRY_DSN,
+  integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: [ENV.PUBLIC_URL],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
