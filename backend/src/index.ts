26a89c7100841b181f7313edf3b88a57e35fa11f
avaice <dev@cho-ice.xyz>
2024-04-12 15:05:02 +0900

    バックエンドからSentry削除

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -24,31 +24,7 @@ const apiLimiter = rateLimit({
 });
 app.use(apiLimiter);
 
-if (!ENV.DEV) {
-  Sentry.init({
-    dsn: ENV.SENTRY_DSN,
-    integrations: [
-      // enable HTTP calls tracing
-      new Sentry.Integrations.Http({ tracing: true }),
-      // enable Express.js middleware tracing
-      new Sentry.Integrations.Express({ app }),
-      nodeProfilingIntegration(),
-    ],
-    // Performance Monitoring
-    tracesSampleRate: 1.0, //  Capture 100% of the transactions
-    // Set sampling rate for profiling - this is relative to tracesSampleRate
-    profilesSampleRate: 1.0,
-  });
-
-  // The request handler must be the first middleware on the app
-  app.use(Sentry.Handlers.requestHandler());
-
-  // TracingHandler creates a trace for every incoming request
-  app.use(Sentry.Handlers.tracingHandler());
-}
-
 app.get('/', (req, res) => {
-  throw new Error('test');
   res.send('Online Reversi Server');
 });
 
