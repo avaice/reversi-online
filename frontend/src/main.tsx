bd7a785dc9ff381daf3b7a9f173997a55bbfb7a0
avaice <dev@cho-ice.xyz>
2024-04-12 14:34:07 +0900

    throw new test error

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -17,4 +17,6 @@ Sentry.init({
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
 });
 
+throw new Error('This is a test error');
+
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
