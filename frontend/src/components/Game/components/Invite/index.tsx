0d57d8c3c493d841f53f3c00c25c846b0632ad2f
avaice <80768507+avaice@users.noreply.github.com>
2024-04-12 15:38:01 +0900

    Feat/a11y improvement (#5)

--- frontend/src/components/Game/components/Invite/index.tsx
+++ frontend/src/components/Game/components/Invite/index.tsx
@@ -18,7 +18,11 @@ export const Invite = ({ shareLink }: { shareLink: string }) => {
         <span className={styles.inviteMessage}>対戦相手にURLを共有してください！</span>
         <div className={styles.copieableBox}>
           <input type="text" readOnly value={shareLink} />
-          <button onClick={handleCopy} disabled={copyButtonText !== 'コピー'}>
+          <button
+            onClick={handleCopy}
+            disabled={copyButtonText !== 'コピー'}
+            aria-label="URLをコピー"
+          >
             {copyButtonText}
           </button>
         </div>
--- frontend/src/components/Loading/Loading.tsx
+++ frontend/src/components/Loading/Loading.tsx
@@ -2,7 +2,7 @@ import styles from './Loading.module.css';
 
 export const Loading = ({ msg, fill }: { msg: string; fill?: boolean }) => {
   return (
-    <div className={`${styles.loading} ${fill ? styles.fill : ''}`}>
+    <div className={`${styles.loading} ${fill ? styles.fill : ''}`} aria-live="assertive">
       <div className={styles.loadingSpinner} />
       <p>{msg}</p>
     </div>
