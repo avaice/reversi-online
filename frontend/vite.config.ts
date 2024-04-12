68539af898bfdc86c2c096b3f7627eff7c0556ef
avaice <dev@cho-ice.xyz>
2024-04-12 20:56:16 +0900

    ビルドコードの分割

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,21 +1,31 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { sentryVitePlugin } from '@sentry/vite-plugin';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   build: {
     sourcemap: true, // Source map generation must be turned on
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          sentry: ['@sentry/react'],
+          socketio: ['socket.io-client'],
+        },
+      },
+      plugins: [
+        htmlPlugin(loadEnv(mode, '.')),
+        sentryVitePlugin({
+          authToken: process.env.SENTRY_AUTH_TOKEN,
+          org: 'greatmyproducts',
+          project: 'online-reversi-frontend',
+        }),
+        visualizer(),
+      ],
+    },
   },
-  plugins: [
-    react(),
-    htmlPlugin(loadEnv(mode, '.')),
-    sentryVitePlugin({
-      authToken: process.env.SENTRY_AUTH_TOKEN,
-      org: 'greatmyproducts',
-      project: 'online-reversi-frontend',
-    }),
-  ],
+  plugins: [react()],
 }));
 
 /**
