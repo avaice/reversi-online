2559af876db9cb45c2e35c900640866d04d8f0e8
avaice <dev@cho-ice.xyz>
2024-04-12 14:42:18 +0900

    DEVモードのみSentryと接続する

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,16 +5,18 @@ import 'the-new-css-reset/css/reset.css';
 import './index.css';
 import { ENV } from './modules/env.ts';
 
-Sentry.init({
-  dsn: ENV.SENTRY_DSN,
-  integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
-  // Performance Monitoring
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: [/localhost:*/, 'online-reversi.xyz'],
-  // Session Replay
-  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-});
+if (!ENV.DEV_MODE) {
+  Sentry.init({
+    dsn: ENV.SENTRY_DSN,
+    integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
+    // Performance Monitoring
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
+    // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+    tracePropagationTargets: [/localhost:*/, 'online-reversi.xyz'],
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  });
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
