27a1a491c90ec419d346135d58db26a5eec371f4
avaice <dev@cho-ice.xyz>
2024-04-12 14:30:19 +0900

    add sentryVitePlugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,21 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  plugins: [react(), htmlPlugin(loadEnv(mode, '.'))],
+  build: {
+    sourcemap: true, // Source map generation must be turned on
+  },
+  plugins: [
+    react(),
+    htmlPlugin(loadEnv(mode, '.')),
+    sentryVitePlugin({
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      org: 'greatmyproducts',
+      project: 'online-reversi-frontend',
+    }),
+  ],
 }));
 
 /**
