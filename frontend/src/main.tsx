82faf035a36cc45e0de50c43173e96f917c67024
avaice <80768507+avaice@users.noreply.github.com>
2024-04-12 14:10:30 +0900

    Fix/Sentry トレース先の修正 (#2)
    * フロントエンドにSentryを追加
    
    * npx @sentry/wizard@latest -i sourcemaps
    
    * トレースターゲットの修正

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -11,7 +11,7 @@ Sentry.init({
   // Performance Monitoring
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
   // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: [ENV.PUBLIC_URL],
+  tracePropagationTargets: [/localhost:*/, new RegExp(ENV.PUBLIC_URL + '*')],
   // Session Replay
   replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
   replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
